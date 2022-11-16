387c713fcced1c2e1b06b0e26e909b807b9321b4
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-11-17 05:11:48 +0700

    adding calender

--- app/src/main/java/com/finpro/garudanih/view/auth/RegisterActivity.kt
+++ app/src/main/java/com/finpro/garudanih/view/auth/RegisterActivity.kt
@@ -1,22 +1,60 @@
 package com.finpro.garudanih.view.auth
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.finpro.garudanih.databinding.ActivityRegisterBinding
+import android.widget.DatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegisterBinding
+    var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.dummy.text = "--/--/----"
+
+
+
+        // create an OnDateSetListener
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+        binding.ivCalender.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@RegisterActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+        })
+
 
         binding.ivBack.setOnClickListener {
             val toLogin = Intent(this,LoginActivity::class.java)
             startActivity(toLogin)
         }
+
+    }
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        binding.dummy.text= sdf.format(cal.getTime())
     }
 }

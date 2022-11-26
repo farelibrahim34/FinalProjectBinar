dfa467e3098bf8ea47101cc663e280c10b6bfdbd
farelibrahim34 <105421371+farelibrahim34@users.noreply.github.com>
2022-11-26 19:05:26 +0700

    pindah profil ke setting

--- app/src/main/java/com/finpro/garudanih/view/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/finpro/garudanih/view/fragments/settings/SettingsFragment.kt
@@ -1,10 +1,20 @@
 package com.finpro.garudanih.view.fragments.settings
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import com.finpro.garudanih.R
 import com.finpro.garudanih.databinding.FragmentSettingsBinding
 
@@ -12,6 +22,8 @@ import com.finpro.garudanih.databinding.FragmentSettingsBinding
 class SettingsFragment : Fragment() {
 
     private lateinit var binding : FragmentSettingsBinding
+    private val REQUEST_CODE_PERMISSION = 100
+    private var imageUri: Uri? = Uri.EMPTY
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +33,86 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivSetImage.setOnClickListener{
+                checkingPermissions()
+        }
+    }
+    private fun checkingPermissions() {
+        if (isGranted(
+                requireActivity(),
+                Manifest.permission.CAMERA,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                REQUEST_CODE_PERMISSION,
+            )
+        ) {
+            chooseImageDialog()
+        }
+    }
+    private fun isGranted(
+        activity: Activity,
+        permission: String,
+        permissions: Array<String>,
+        request: Int,
+    ): Boolean {
+        val permissionCheck = ActivityCompat.checkSelfPermission(activity, permission)
+        return if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                showPermissionDeniedDialog()
+            } else {
+                ActivityCompat.requestPermissions(activity, permissions, request)
+            }
+            false
+        } else {
+            true
+        }
+    }
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Permission Denied")
+            .setMessage("Permission is denied, Please allow permissions from App Settings.")
+            .setPositiveButton(
+                "App Settings"
+            ) { _, _ ->
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri = Uri.fromParts("package", "", null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+            .show()
+    }
+    private fun chooseImageDialog() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("Pilih Gambar")
+            .setPositiveButton("Gallery") { _, _ -> openGallery() }
+            .show()
+    }
+    private fun openGallery() {
+        Intent().type = "image/*"
+        galleryResult.launch("image/*")
+    }
+    private fun handleCameraImage(intent: Intent?) {
+        val bitmap = intent?.extras?.get("data") as Bitmap
+        binding.ivSetImage.setImageBitmap(bitmap)
+    }
+    private val cameraResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                handleCameraImage(result.data)
+            }
+        }
+    private val galleryResult =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { result ->
+            imageUri = result
+            binding.ivSetImage.setImageURI(result)
+        }
 }

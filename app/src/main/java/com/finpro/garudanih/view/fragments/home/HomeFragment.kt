9a0e6dcc2903bd7a26f98f91248e89586a6f9753
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-12-04 23:06:38 +0700

    04/12/2022

--- app/src/main/java/com/finpro/garudanih/view/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/finpro/garudanih/view/fragments/home/HomeFragment.kt
@@ -102,14 +102,15 @@ class HomeFragment : Fragment() {
     private fun setUpTiketInt(){
         val viewModel = ViewModelProvider(requireActivity()).get(TiketViewModel::class.java)
         viewModel.getLdTiket().observe(viewLifecycleOwner) {
+            binding.homeProgressBar.visibility = View.VISIBLE
             if (it != null) {
-                binding.homeProgressBar.visibility = View.VISIBLE
-                binding.rvLocal.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+                binding.homeProgressBar.visibility = View.GONE
+                binding.rvLocal.layoutManager =
+                    LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
                 tiketAdapter = AdapterTiket(it.data.tickets)
                 binding.rvLocal.adapter = tiketAdapter
                 Toast.makeText(requireActivity(), "Data Tampil", Toast.LENGTH_SHORT).show()
             } else {
-                binding.homeProgressBar.visibility = View.GONE
                 Toast.makeText(requireActivity(), "Data Tidak Tampil", Toast.LENGTH_SHORT).show()
             }
         }
--- app/src/main/java/com/finpro/garudanih/view/fragments/order/OrderFragment.kt
+++ app/src/main/java/com/finpro/garudanih/view/fragments/order/OrderFragment.kt
@@ -1,22 +1,36 @@
 package com.finpro.garudanih.view.fragments.order
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.finpro.garudanih.R
+import com.finpro.garudanih.databinding.FragmentOrderBinding
+import com.finpro.garudanih.databinding.FragmentSuccessOrderBinding
+import com.finpro.garudanih.view.fragments.success.SuccessOrderFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OrderFragment : Fragment() {
 
+    private lateinit var binding : FragmentOrderBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        binding = FragmentOrderBinding.inflate(layoutInflater, container, false)
         return inflater.inflate(R.layout.fragment_order, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCari.setOnClickListener {
+            startActivity(Intent(context, SuccessOrderFragment::class.java))
+        }
     }
 }

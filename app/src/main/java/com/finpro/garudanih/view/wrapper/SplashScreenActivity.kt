be59304828b76794aaf7fc32bcce6ff6579e72e6
farelibrahim34 <105421371+farelibrahim34@users.noreply.github.com>
2022-12-28 20:29:57 +0700

    benerin splashscreen

--- app/src/main/java/com/finpro/garudanih/view/wrapper/SplashScreenActivity.kt
+++ app/src/main/java/com/finpro/garudanih/view/wrapper/SplashScreenActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.finpro.garudanih.databinding.ActivitySplashScreenBinding
 import com.finpro.garudanih.utils.CheckUserUtil
@@ -24,20 +25,42 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         authViewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
-        val background = object : Thread() {
-            override fun run() {
-                try {
-                    Thread.sleep(5000)
-                    val intent = Intent(baseContext, HomeBottomActivity::class.java)
-                    startActivity(intent)
-                    sudahlogin()
-                    finish()
-                }catch (e : Exception){
-                    e.printStackTrace()
-                }
-            }
-        }
-        background.start()
+//        val background = object : Thread() {
+//            override fun run() {
+//                    Thread.sleep(5000)
+////                    val intent = Intent(baseContext, HomeBottomActivity::class.java)
+////                    startActivity(intent)
+////                    sudahlogin()
+////                    finish()
+//                    authViewModel.getToken().observe(this@SplashScreenActivity){
+//                        if (it != null){
+//                            val validasi = CheckUserUtil.validateUser(it)
+//                            if (validasi){
+//                                startActivity(Intent(baseContext, HomeBottomActivity::class.java))
+//                            }else{
+//                                startActivity(Intent(baseContext, ViewpagerActivity::class.java))
+//                            }
+//                        }
+//                    }
+//                    authViewModel.getNoHp().observe(this@SplashScreenActivity){
+//                        if (it != null){
+//                            if (!it.equals("undefined")){
+//                                startActivity(Intent(baseContext, HomeBottomActivity::class.java))
+//                            }else{
+//                                startActivity(Intent(baseContext, ViewpagerActivity::class.java))
+//                            }
+//                        }
+//                    }
+//
+//
+//            }
+//        }
+//        background.start()
+
+        Handler().postDelayed({
+            sudahlogin()
+            finish()
+        },5000)
     }
     private fun sudahlogin(){
         authViewModel.getToken().observe(this){
